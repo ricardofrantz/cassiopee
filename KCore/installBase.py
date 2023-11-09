73e2b43c0fac9b8f8850c36918aa4b9ba0c1dcb5
R Frantz <rasfrantz@gmail.com>
2023-11-09 12:47:41 +0100

    fix

--- KCore/installBase.py
+++ KCore/installBase.py
@@ -1,3 +1,11 @@
+import os
 import sys
-sys.path.insert(0, '../../../')
+
+# Get the path from an environment variable
+cassiopee_path = os.getenv('cassiopee_src_path')
+
+if cassiopee_path is not None:
+    # Add the path to sys.path
+    sys.path.insert(0, cassiopee_path)
+
 from machineConfig import installDict
--- machineConfig.py
+++ machineConfig.py
@@ -14,6 +14,22 @@ False, # CPlotOffScreen
 ['/usr/lib/x86_64-linux-gnu/hdf5/serial/'], # additionalLibPaths
 False, # useCuda
 [] # NvccAdditionalOptions
-]
+],
+###############################################################################
+# 'pop-os': [ 'cobra',
+# 'gfortran', # f77compiler
+# 'gfortran', # f90compiler
+# 'gcc', # Cppcompiler
+# [], # CppAdditionalOptions
+# [], # f77AdditionalOptions
+# True, # useOMP
+# False, # static
+# False, # CPlotOffScreen
+# ['/usr/include/mpi', '/usr/include/hdf5/serial'], # additionalIncludePaths
+# ['gfortran', 'gomp', 'pthread'], # additionalLibs
+# ['/usr/lib/x86_64-linux-gnu','/usr/lib/x86_64-linux-gnu/hdf5/serial/'], # additionalLibPaths
+# False, # useCuda
+# [] # NvccAdditionalOptions
+# ]
 ###############################################################################
 }
