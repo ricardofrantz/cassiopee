e33d7b71f18d8c840a71277557ac1f62a776a6fb
R Frantz <rasfrantz@gmail.com>
2023-11-09 12:55:09 +0100

    updates

--- KCore/installPath.py
+++
@@ -1,3 +0,0 @@
-installPath = '/home/afranchini/CASSIOPEE/3.6//Dist/bin/x86_r8/lib/python3.8/site-packages'
-libPath = '/home/afranchini/CASSIOPEE/3.6//Dist/bin/x86_r8/lib'
-includePath = '/home/afranchini/CASSIOPEE/KCore'
